2eb888781e0097488bc501fbef56f027d034fcc4
Hargobind Atwal <ethaipath@gmail.com>
2019-02-22 16:33:33 -0800

    Got the solution

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -108,78 +108,48 @@ class SortingRobot:
         to on
 
         set light on
-        pick up first item
         while light is on
             set light off
             pick up first item
-            move right
+            move to start of list
+            while can move right
+                move right
+                if 
 
         """
 
         # Fill this out
 
-        self.set_light_on()
         print('inital state: ', self._list)
         # self.swap_item()
-        while self.light_is_on():
-            print('state: ', self._list, 'robo pos: ', self._position, 'item: ', self._item, '', 'light: ', self.light_is_on())
 
-            self.set_light_off()
-            while self.can_move_left(): # go to start
-                self.move_left()
-            self.swap_item()
-
+        while not self.light_is_on():
+            # print('state: ', self._list, 'robo pos: ', self._position, 'item: ', self._item, '', 'light: ', self.light_is_on())
+            self.set_light_on()
             while self.can_move_right():
-                print(self._list, 'robo pos: ', self._position, 'item: ', self._item, '', 'light: ', self.light_is_on())
+                # print('t: ', self._time, self._list, 'robo pos: ', self._position, 'item: ', self._item, '', 'light: ', self.light_is_on())
+                self.swap_item()
                 self.move_right()
-                if self.compare_item() == 1:
-                    self.set_light_on()
-                    self.swap_item()
-                    self.move_left()
-                    self.swap_item()
-                    self.move_right()
-                    self.swap_item()
-                elif self.compare_item() == -1:
-                    # self.set_light_on()
-                    self.move_left()
-                    self.swap_item()
-                    self.move_right()
-                    self.swap_item()
-
-                if not self.can_move_right() and (self.compare_item() == 1 or self.compare_item() == None):
-                    # self.set_light_on()
-                    self.swap_item()
-
-                # elif not self.can_move_right() and self.compare_item() == -1:
-                #     print('break')
-                #     break
-
-                # elif self.compare_item() == None and not self.light_is_on():
+                # if self.compare_item() == 0:
                 #     self.swap_item()
-
-
-        # print('inital state: ', self._list)
-        # while self.can_move_right():
-        #     self.move_right()
-        # self.set_light_on()
-        # self.swap_item()
-        # while self.light_is_on():
-        #     print('robo pos: ', self._position, 'item: ', self._item, '', 'light: ',self.light_is_on(),'state: ', self._list)
-        #     self.set_light_off()
-        #     while self.can_move_left():
-        #         self.move_left()
-        #         if self.compare_item() == -1:
-        #             self.set_light_on()
-        #             self.swap_item()
-        #             self.move_left()
-        #             self.swap_item()
-        #             self.move_left()
-        #     while self.can_move_right():
-        #         self.move_right()
-
-            # while self.can_move_left(): # go to start
-            #     self.move_left()
-
+                #     self.set_light_on()
+                #     self.move_left()
+                #     self.swap_item()
+                #     self.move_right()
+                if self.compare_item() > 0:
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    self.set_light_off()
+                else:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+            if not self.light_is_on():
+                while self.can_move_left(): # go to start
+                    self.move_left()
+        # print('end state: ', self._list)
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
@@ -190,8 +160,8 @@ if __name__ == "__main__":
     # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     # l = [5, 4, 3, 2, 1]
-    # l = [5, 3, 4, 3, 2, 1]
-    l = [2, 1, 3, 5, 4]
+    l = [5, 3, 4, 3, 2, 1]
+    # l = [2, 1, 3, 5, 4]
     robot = SortingRobot(l)
 
     robot.sort()
